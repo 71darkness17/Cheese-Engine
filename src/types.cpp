94b9d0cad0c4a89ac26e40a7d99dc31fcb1956ed
Dmitriy Lepa <dalepa@edu.hse.ru>
2026-05-31 12:36:51 +0300

    phys2d: implement AABB functionality - added AABB constructors and getters - added AABB collision
    signed-off-by: Lepa Dmitriy <dalepa@edu.hse.ru>

---
+++ src/types.cpp
@@ -0,0 +1,32 @@
+#include <types.hpp>
+#include <glm/glm.hpp>
+
+phys2d::AABB::AABB(
+  const glm::vec2& lp, 
+  const glm::vec2& ub): lowerBound(lp), upperBound(ub)
+{
+
+}
+
+const glm::vec2 phys2d::AABB::getLowerBound() const 
+{
+  return lowerBound;
+}
+
+const glm::vec2 phys2d::AABB::getUpperBound() const 
+{
+  return upperBound;
+}
+
+bool phys2d::AABB::collidesWith(const AABB& other)
+{
+  glm::vec2 left_upper = other.getUpperBound();
+  glm::vec2 right_lower = other.getLowerBound();
+  glm::vec2 left_lower(left_upper.x, right_lower.y);
+  glm::vec2 right_upper(right_lower.x, left_upper.y);
+
+  return getLowerBound().x <= other.getUpperBound().x &&
+         getUpperBound().x >= other.getLowerBound().x &&
+         getLowerBound().y <= other.getUpperBound().y &&
+         getUpperBound().y >= other.getLowerBound().y;
+}
