ebb79188d70e534a2bf2eb557efbd43e2adb07d6
Vladimir Zavorokhin <vvzavorokhin@edu.hse.ru>
2026-06-13 12:29:44 +0300

    graphics: refactor code due to clang-format
    Signed-off-by: Vladimir Zavorokhin <vvzavorokhin@edu.hse.ru>

--- inc/renderQueue.hpp
+++ inc/renderQueue.hpp
@@ -2,64 +2,57 @@
 
 #define GLM_FORCE_RADIANS
 
-#include <glm/glm.hpp>
-
-#include <vector>
-#include <cstdlib>
 #include <algorithm>
 #include <array>
+#include <cstdlib>
+#include <glm/glm.hpp>
 #include <mutex>
 #include <queue>
+#include <vector>
 
-enum RenderCommandType {
-    AddRect,
-    AddTriangle,
-    RemoveFig,
-    SetTransform,
-    SetCamera
-};
+enum RenderCommandType { AddRect, AddTriangle, RemoveFig, SetTransform, SetCamera };
 
 struct RenderCommand {
-    RenderCommandType type;
+  RenderCommandType type;
 
-    union {
-        struct {
-            glm::vec2 position;
-            float width;
-            float height;
-            glm::vec3 color;
-            uint32_t index;
-        } addRect;
+  union {
+    struct {
+      glm::vec2 position;
+      float width;
+      float height;
+      glm::vec3 color;
+      uint32_t index;
+    } addRect;
 
-        struct {
-            std::array<glm::vec2, 3> positions;
-            glm::vec3 color;
-            uint32_t index;
-        } addTri;
+    struct {
+      std::array<glm::vec2, 3> positions;
+      glm::vec3 color;
+      uint32_t index;
+    } addTri;
 
-        struct {
-            uint32_t index;
-        } remove;
+    struct {
+      uint32_t index;
+    } remove;
 
-        struct {
-            uint32_t index;
-            glm::mat4 model;
-        } transform;
+    struct {
+      uint32_t index;
+      glm::mat4 model;
+    } transform;
 
-        struct {
-            glm::vec2 position;
-            float zoom;
-        } camera;
-    };
+    struct {
+      glm::vec2 position;
+      float zoom;
+    } camera;
+  };
 };
 
 class RenderQueue {
 public:
-    void push(RenderCommand command);
+  void push(RenderCommand command);
 
-    bool pop(RenderCommand& command);
+  bool pop(RenderCommand& command);
 
 private:
-    std::mutex mutex;
-    std::queue<RenderCommand> queue;
+  std::mutex mutex;
+  std::queue<RenderCommand> queue;
 };
